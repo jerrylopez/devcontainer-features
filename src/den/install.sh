b8d7d70a26f8df466fb5326f2fe262608e930ebc
Jerry Lopez <me@jerrylopez.dev>
2022-11-26 15:26:03 +0000

    Switch den feature to use jerrylopez/den

--- src/den/install.sh
+++ src/den/install.sh
@@ -3,7 +3,7 @@ set -e
 
 sudo mkdir /opt/den
 sudo chown $(whoami) /opt/den
-git clone https://github.com/swiftotter/den.git --branch ${VERSION:-"1.0.0-beta.12"} /opt/den
+git clone https://github.com/jerrylopez/den.git --branch ${VERSION:-"1.0.0-beta.12"} /opt/den
 
 sudo ln -s /opt/den/bin/den /usr/local/bin/den
 sudo chown $(whoami) /usr/local/bin/den
